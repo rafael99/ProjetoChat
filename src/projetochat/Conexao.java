69bd429da27bf494826fe2c9d13920d231033582
Marcelo Sousa <mar.sousa2061@gmail.com>
2019-10-15 18:28:01 -0300

    Dia 2

--- src/projetochat/Conexao.java
+++ src/projetochat/Conexao.java
@@ -4,23 +4,24 @@ import com.sun.istack.internal.logging.Logger;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import sun.util.logging.PlatformLogger;
 
 public class Conexao extends Thread{
     Socket cliente;
+    static ArrayList<String> lista_usuarios = new ArrayList<String>();
+
     
     Conexao(Socket conexao) {
         this.cliente = conexao;
+        start();
     }
     
     @Override
-    public void run() {
-        // System.out.println("Nova conexão com o cliente "+ cliente.getInetAddress().getHostAddress());
-        
+    public void run() {        
         Scanner entrada = null;
-        
         try {
             entrada = new Scanner(cliente.getInputStream());
         } catch(IOException ex) {
@@ -38,9 +39,50 @@ public class Conexao extends Thread{
         
         while(entrada.hasNextLine()) {
             String msg = entrada.nextLine();
+            saida.println("Você digitou: " + msg);
+            // Pegando a ação do usuario
+            String[] protocolo = msg.split(":");
+            switch (protocolo[0]) {
+                case "login":
+                    if (login(protocolo[1])) {
+                        saida.println("O nome de usuário foi registrado com sucesso");
+                    } else {
+                        saida.println("O nome do usuário não pôde ser "
+                            + "registrado pois é inválido ou já está em uso");
+                    }
+                    break;
+                case "lista_usuarios":
+                    listarUsuarios();
+                    break;
+                case "mensagem":
+                    break;
+                case "transmitir":
+                    break;
+                default:
+                    System.out.println("Protocolo " + protocolo[0] + " não existe!");
+                    break;
+            }
+            
             System.out.println("O cliente digitou: " + msg);
             
-            saida.println("Você digitou: " + msg);
         }
+    }
+    
+    public static boolean login(String usuario) {
+        for (String user : lista_usuarios) {
+            if (user.equals(usuario)) {
+                return false;
+            }
+        }
+        lista_usuarios.add(usuario);
+        return true;
+    }
+    
+    public static String listarUsuarios() {
+        String usuarios = null;
+        for (String user : lista_usuarios) {
+            usuarios += user + ";";
+        }
+        return usuarios;
     }
 }
--- src/projetochat/Servidor.java
+++ src/projetochat/Servidor.java
@@ -10,69 +10,18 @@ import java.util.Scanner;
 
 public class Servidor {
 
-    static ArrayList<String> lista_usuarios = new ArrayList<String>();
-    ;
     static String remetente;
     static String destinatario;
 
     public static void main(String[] args) throws UnknownHostException, IOException {
-        ServerSocket servidor = new ServerSocket(2424);
-
+        ServerSocket servidor = new ServerSocket(2424);        
         System.out.println("Porta 2424 aberta! Aguardando conexão!");
 
         while (true) {
 
-            Socket cliente = servidor.accept();
-            Scanner usuario = new Scanner(cliente.getInputStream());
-            
-            // Saídas para o cliente
-            PrintStream saida = new PrintStream(cliente.getOutputStream());
-            PrintStream saidaListaUsuarios = new PrintStream(cliente.getOutputStream());
-            
-            // Pegando a ação do usuario
-            String[] protocolo = usuario.nextLine().split(":");
-            
-            switch (protocolo[0]) {
-                case "login":
-                    if (login(protocolo[1])) {
-                        Conexao cnx = new Conexao(cliente);
-                        cnx.start();
-                        System.out.println("Usuário "+ protocolo[1] + " conectado!");
-                        saida.println("Você está conectado!");
-                    } else {
-                        System.out.println("Nome já existente!");
-                    }
-                    break;
-                case "lista_usuarios":
-                    listarUsuarios();
-                    break;
-                case "mensagem":
-                    break;
-                case "transmitir":
-                    break;
-                default:
-                    System.out.println("Protocolo " + protocolo[0] + " não existe!");
-                    break;
-            }
+            Socket cliente = servidor.accept();            
+            new Conexao(cliente);
         }
 
-    }
-
-    public static boolean login(String usuario) {
-        for (String user : lista_usuarios) {
-            if (user == usuario) {
-                return false;
-            }
-        }
-        lista_usuarios.add(usuario);
-        return true;
-    }
-    
-    public static String listarUsuarios() {
-        String usuarios = null;
-        for (String user : lista_usuarios) {
-            usuarios += user + ";";
-        }
-        return usuarios;
     }
 }
